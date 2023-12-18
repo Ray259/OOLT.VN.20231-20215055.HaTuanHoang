e07ae020a05cf9254dad2c904bb6a4e56bed4a7b
Ray <elninothepea@gmail.com>
2023-12-19 01:12:39 +0700

    add javafx prj

--- GUIProject/src/hust/soict/hedspi/javafx/PainterController.java
+++ GUIProject/src/hust/soict/hedspi/javafx/PainterController.java
@@ -1,23 +1,57 @@
 package hust.soict.hedspi.javafx;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Accordion;
+import javafx.scene.control.CheckBox;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 public class PainterController {
-    @FXML
-    private Pane drawingAreaPane;
 
-    @FXML
-    private void clearButtonPressed() {
-        drawingAreaPane.getChildren().clear();
-    }
+	private boolean pen;
+	private Color currentColor = Color.BLACK;
+	@FXML
+	private Pane drawingAreaPane;
 
-    @FXML
-    private void drawingAreaMouseDragged(MouseEvent event) {
-        Circle newCircle = new Circle(event.getX(), event.getY(), 4, Color.BLACK);
-        drawingAreaPane.getChildren().add(newCircle);
-    }
+	@FXML
+	private CheckBox penBox;
+
+	@FXML
+	private CheckBox eraseBox;
+
+	@FXML
+	private void clearButtonPressed() {
+		drawingAreaPane.getChildren().clear();
+	}
+
+	@FXML
+	private void drawingAreaMouseDragged(MouseEvent event) {
+		Circle newCircle = new Circle(event.getX(), event.getY(), 4, currentColor);
+		drawingAreaPane.getChildren().add(newCircle);
+	}
+
+	@FXML
+	private void penCheckBoxAction(ActionEvent action) {
+		if (penBox.isSelected()) {
+			eraseBox.setSelected(false);
+			currentColor = Color.BLACK;
+		} else {
+			eraseBox.setSelected(true);
+			currentColor = Color.WHITE;
+		}
+	}
+
+	@FXML
+	private void eraserCheckBoxAction(ActionEvent action) {
+		if (eraseBox.isSelected()) {
+			penBox.setSelected(false);
+			currentColor = Color.WHITE;
+		} else {
+			penBox.setSelected(true);
+			currentColor = Color.BLACK;
+		}
+	}
 }
